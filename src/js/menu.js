e11b8ed9fc80fdaf10c4dcf86f7b16bf45dddfb2
Artem Safin <artemsafin.webdev@gmail.com>
2021-06-30 19:28:00 +0300

    Правки

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,4 +1,8 @@
 import { clearAllBodyScrollLocks, disableBodyScroll } from 'body-scroll-lock';
+  
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
 export default function menu() {
     const btn = document.querySelector('.page-header__burger');
@@ -32,5 +36,53 @@ export default function menu() {
         } else {
             closeMenu();
         }
+    });
+
+    if (!window.matchMedia('(max-width: 640px)').matches) return;
+
+    const elements = Array.from(document.querySelectorAll('.page-header__nav-link--has-dropdown'));
+    const SPEED = 0.7;
+    console.log('Elements', elements);
+    const openAccordion = element => {
+        gsap.to(element, {
+            height: 'auto',
+            duration: SPEED,
+            onComplete: () => ScrollTrigger.refresh()
+        });
+    };
+    const closeAccordion = element => {
+        gsap.to(element, {
+            height: 0,
+            duration: SPEED,
+            onComplete: () => ScrollTrigger.refresh()
+        });
+    };
+
+    elements.forEach(element => {
+        const btn = element.querySelector('.page-header__nav-link-inner');
+        const content = element.querySelector('.page-header__nav-link-dropdown');
+
+        btn.addEventListener('click', event => {
+            event.preventDefault();
+
+            if (element.hasAttribute('data-close-other')) {
+                elements.forEach(otherElement => {
+                    if (otherElement !== element) {
+                        if (otherElement.classList.contains('active')) {
+                            const content = otherElement.querySelector('.page-header__nav-link-dropdown');
+                            closeAccordion(content);
+                            otherElement.classList.remove('active');
+                        }
+                    }
+                });
+            }
+
+            if (element.classList.contains('active')) {
+                closeAccordion(content);
+            } else {
+                openAccordion(content);
+            }
+            element.classList.toggle('active');
+        });
     });
 }
