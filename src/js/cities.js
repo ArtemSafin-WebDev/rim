f4314059a377399e23f9df87a0e93b0db20abbac
gilyazov <gilyazov.aydar@gmail.com>
2024-11-02 16:48:36 +0300

    валидация

--- src/js/cities.js
+++ src/js/cities.js
@@ -1,5 +1,7 @@
 import phoneMask from './utils/phoneMask';
+import validation from './utils/validation';
 
 document.addEventListener('DOMContentLoaded', function() {
     phoneMask();
+    validation();
 });
